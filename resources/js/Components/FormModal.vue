4a9e9f0884a9293e4d3fc49634c6601f7fed9e4d
Luke Downing <lukeraymonddowning@gmail.com>
2022-09-14 17:19:27 +0100

    Parrots!

--- resources/js/Components/FormModal.vue
+++ resources/js/Components/FormModal.vue
@@ -6,7 +6,7 @@ import {
     TransitionRoot,
     TransitionChild,
 } from "@headlessui/vue";
-import { reactive } from "vue";
+import {nextTick, reactive, ref, watch} from "vue";
 
 const props = defineProps({
     title: {
@@ -24,6 +24,17 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["close"]);
+
+const renderParrot = ref(false);
+const showParrot = ref(false);
+
+watch(props, async (props) => {
+    if (!props.active) {
+        return;
+    }
+
+    setTimeout(() => showParrot.value = true, 500);
+}, {deep: true});
 </script>
 
 <template>
@@ -45,8 +56,14 @@ const emit = defineEmits(["close"]);
 
             <div class="fixed inset-0 z-10 overflow-y-auto">
                 <div
-                    class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+                    class="flex relative min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
                 >
+                    <div v-show="renderParrot"
+                         class="absolute w-32 top-16 left-28 -rotate-90 duration-1000"
+                         :class="{ '-translate-x-24': showParrot }"
+                    >
+                        <img src="/assets/angry-parrot.png" alt="" class=""/>
+                    </div>
                     <TransitionChild
                         as="template"
                         enter="ease-out duration-300"
@@ -55,6 +72,8 @@ const emit = defineEmits(["close"]);
                         leave="ease-in duration-200"
                         leave-from="opacity-100 translate-y-0 sm:scale-100"
                         leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        @after-enter="renderParrot = true"
+                        @beforeLeave="showParrot = false"
                     >
                         <DialogPanel
                             class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-5xl"
@@ -67,7 +86,8 @@ const emit = defineEmits(["close"]);
                                         <DialogTitle
                                             as="h3"
                                             class="text-lg font-medium leading-6 text-gray-900"
-                                            >{{ props.title }}</DialogTitle
+                                        >{{ props.title }}
+                                        </DialogTitle
                                         >
                                         <div
                                             v-if="props.description"
@@ -77,14 +97,14 @@ const emit = defineEmits(["close"]);
                                                 {{ props.description }}
                                             </p>
                                         </div>
-                                        <slot name="form" />
+                                        <slot name="form"/>
                                     </div>
                                 </div>
                             </div>
                             <div
                                 class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
                             >
-                                <slot name="actions" />
+                                <slot name="actions"/>
                             </div>
                         </DialogPanel>
                     </TransitionChild>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,6 +9,9 @@ import { Head, Link } from "@inertiajs/inertia-vue3";
     <div
         class="items-top relative flex min-h-screen justify-center bg-teal-400 dark:bg-gray-900 sm:items-center sm:pt-0"
     >
-        <Link :href="route('movies.index')" class="text-3xl uppercase bg-black text-white py-6 px-16 rounded hover:bg-gray-900">Let's Go</Link>
+        <div class="group relative flex">
+            <img src="/assets/angry-parrot.png" class="absolute w-full object-contain top-1 h-16 z-0 group-hover:-translate-y-16 transform duration-500">
+            <Link :href="route('movies.index')" class="z-10 text-3xl uppercase bg-black text-white py-6 px-16 rounded hover:bg-gray-900">Let's Go</Link>
+        </div>
     </div>
 </template>
